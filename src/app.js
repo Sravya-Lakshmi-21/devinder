15c8469133c12ed9e948426a3acca5ef54090aa9
Sravya <sravyalakshmi21@gmail.com>
2026-06-09 19:13:35 +0530

    using middleware for authorization

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
 const express = require("express");
 
+//const {adminAuth} = require("./middleware/admin")
+
 const app = express();
 
-app.use("/home",
-    (req,res, next)=>{
-        console.log("1st Home");
-    //res.send("Hello from home1");
-    next();
+app.use("/admin", (req,res, next)=>{
+        const token = "fdf";
+        const isAuthorized = token == "xyz";
+        console.log("token authorized");
+        if(isAuthorized){
+            //res.send("User data Sent");
+            next();
+        }
+        else{
+            res.status(401).send("Not AUthorized");
+        }
    
-},
-(req, res, next)=>{
-     console.log("2nd Home");
-    res.send("Hello from home2");
-    next();
+});
+
+app.get("/admin/getUserData",
+    (req,res, next)=>{
+        res.send("User data retrieved");
+   
+});
+app.post("/admin/createUserData",
+    (req,res, next)=>{
+        res.send("User data Created");
+   
+});
+app.delete ("/admin/deleteUserData",
+    (req,res, next)=>{
+        res.send("User data deleted");
+   
+});
+
+app.put ("/admin/updateUserData",
+    (req,res, next)=>{
+        res.send("User data Updated");
+   
 });
 
 app.listen(7777,()=>{
