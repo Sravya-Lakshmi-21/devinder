f7abe8b5375aebbb9dbcdb138298c50c341c2433
Sravya <sravyalakshmi21@gmail.com>
2026-06-10 11:22:12 +0530

    removed secrets

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     await mongoose.connect(
-        'mongodb://myFirstProject:aKTQ1cbDW6Jfaq7C@ac-rc5ukwk-shard-00-00.qu3smby.mongodb.net:27017,ac-rc5ukwk-shard-00-01.qu3smby.mongodb.net:27017,ac-rc5ukwk-shard-00-02.qu3smby.mongodb.net:27017/?ssl=true&replicaSet=atlas-9gx2m5-shard-0&authSource=admin&appName=MyFirstProject/Devinder'
-    );
+        );
 };
 
 module.exports = {
